34a69ec1ad83fc366064437e5566d39b501d3270
Giang Tran <giangtran@Giangs-MacBook-Pro.local>
2021-10-19 22:46:56 +0700

    clean

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
-/* eslint-disable react-native/no-inline-styles */
 import React, {FC, useRef} from 'react';
 import {
   SafeAreaView,
   Text,
   Platform,
   View,
-  Dimensions,
   StyleSheet,
   TouchableOpacity,
   Image,
@@ -15,43 +13,36 @@ import {
 import Svg from 'react-native-svg';
 import {VictoryPie} from 'victory-native';
 import icons from './src/icons';
+import {COLORS} from './src/colors';
+import {categoriesData} from './src/data';
+import {SIZES} from './src/constant';
 interface AppProps {}
-const {width, height} = Dimensions.get('screen');
-export const SIZES = {
-  // global sizes
-  base: 8,
-  font: 14,
-  radius: 12,
-  padding: 24,
-  padding2: 36,
+interface Data {
+  id: number;
+  name: string;
+  icon: any;
+  color: string;
+  expenses: {
+    id: number;
+    title: string;
+    description: string;
+    location: string;
+    total: number;
+    status: string;
+  }[];
+}
 
-  // font sizes
-  largeTitle: 50,
-  h1: 30,
-  h2: 22,
-  h3: 16,
-  h4: 14,
-  body1: 30,
-  body2: 20,
-  body3: 16,
-  body4: 14,
-
-  // app dimensions
-  width,
-  height,
-};
 const App: FC<AppProps> = () => {
-  const [categories, setCategories] = React.useState(categoriesData);
-  const [selectedCategory, setSelectedCategory] = React.useState<any>(null);
-  const [showMoreToggle, setShowMoreToggle] = React.useState(false);
+  const [categories] = React.useState<Data[]>(categoriesData);
+  const [selectedCategory, setSelectedCategory] = React.useState<Data>();
+  const [showMoreToggle, setShowMoreToggle] = React.useState<boolean>(false);
   const categoryListHeightAnimationValue = useRef(
     new Animated.Value(115),
   ).current;
   const processCategoryDataToDisplay = () => {
-    let chartData = categories.map(item => {
+    let chartData = categories.map((item: Data) => {
       let confirmExpenses = item.expenses.filter(a => a.status === 'C');
       let total = confirmExpenses.reduce((a, b) => a + (b.total || 0), 0);
-      console.log('Debug 2----', total);
       return {
         name: item.name,
         y: total,
@@ -63,10 +54,8 @@ const App: FC<AppProps> = () => {
 
     // filter out categories with no data/expenses
     let filterChartData = chartData.filter(a => a.y > 0);
-    console.log('Data check', filterChartData);
     // Calculate the total expenses
     let totalExpense = filterChartData.reduce((a, b) => a + (b.y || 0), 0);
-    console.log('Total', totalExpense);
     // Calculate percentage and repopulate chart data
     let finalChartData = filterChartData.map(item => {
       let percentage = ((item.y / totalExpense) * 100).toFixed(2);
@@ -82,7 +71,7 @@ const App: FC<AppProps> = () => {
 
     return finalChartData;
   };
-  const setSelectCategoryByName = (name: any) => {
+  const setSelectCategoryByName = (name: string) => {
     let category = categories.filter(a => a.name === name);
     setSelectedCategory(category[0]);
   };
@@ -93,29 +82,28 @@ const App: FC<AppProps> = () => {
       (a, b) => a + (b.expenseCount || 0),
       0,
     );
-
-    console.log('Check Chart');
-    console.log(chartData);
     if (Platform.OS === 'ios') {
       return (
-        <View style={{alignItems: 'center', justifyContent: 'center'}}>
+        <View style={styles.viewChart}>
           <VictoryPie
             data={chartData}
             labels={datum => `${datum.y}`}
             radius={({datum}) =>
-              selectedCategory && selectedCategory.name == datum.name
+              selectedCategory && selectedCategory.name === datum.name
                 ? SIZES.width * 0.4
                 : SIZES.width * 0.4 - 10
             }
             innerRadius={70}
-            labelRadius={({innerRadius}) =>
-              (SIZES.width * 0.4 + innerRadius) / 2.5
-            }
+            // labelRadius={({innerRadius}) =>
+            //   (SIZES.width * 0.4 + innerRadius) / 2.5
+            // }
+            labelRadius={(SIZES.width * 0.4 + 70) / 2.5}
             style={{
               labels: {fill: 'white', fontSize: SIZES.body3, lineHeight: 22},
-              parent: {
-                ...styles.shadow,
-              },
+              //Shadow in chart - option
+              // parent: {
+              //   ...styles.shadow,
+              // },
             }}
             width={SIZES.width * 0.8}
             height={SIZES.width * 0.8}
@@ -140,41 +128,38 @@ const App: FC<AppProps> = () => {
             ]}
           />
 
-          <View style={{position: 'absolute', top: '42%', left: '42%'}}>
-            <Text style={{fontSize: SIZES.h1, textAlign: 'center'}}>
-              {totalExpenseCount - 1}
-            </Text>
-            <Text style={{fontSize: SIZES.h3, textAlign: 'center'}}>
-              Chi phí
-            </Text>
+          <View style={styles.centerChart}>
+            <Text style={styles.textH1}>{totalExpenseCount - 1}</Text>
+            <Text style={styles.textH3}>Expenses</Text>
           </View>
         </View>
       );
     } else {
       return (
-        <View style={{alignItems: 'center', justifyContent: 'center'}}>
+        <View style={styles.viewChart}>
           <Svg
             width={SIZES.width}
             height={SIZES.width}
-            style={{width: '100%', height: 'auto'}}>
+            style={styles.svgChartAndroid}>
             <VictoryPie
-              standalone={false} // Android workaround
+              standalone={false} // Android work around
               data={chartData}
               labels={datum => `${datum.y}`}
               radius={({datum}) =>
-                selectedCategory && selectedCategory.name == datum.name
+                selectedCategory && selectedCategory.name === datum.name
                   ? SIZES.width * 0.4
                   : SIZES.width * 0.4 - 10
               }
               innerRadius={70}
-              labelRadius={({innerRadius}) =>
-                (SIZES.width * 0.4 + innerRadius) / 2.5
-              }
+              // labelRadius={({innerRadius}) =>
+              //   (SIZES.width * 0.4 + innerRadius) / 2.5
+              // }
+              labelRadius={(SIZES.width * 0.4 + 70) / 2.5}
               style={{
                 labels: {fill: 'white', fontSize: SIZES.h3},
-                parent: {
-                  ...styles.shadow,
-                },
+                // parent: {
+                //   ...styles.shadow,
+                // },
               }}
               width={SIZES.width}
               height={SIZES.width}
@@ -199,39 +184,27 @@ const App: FC<AppProps> = () => {
               ]}
             />
           </Svg>
-          <View style={{position: 'absolute', top: '42%', left: '42%'}}>
-            <Text style={{fontSize: SIZES.h1, textAlign: 'center'}}>
-              {totalExpenseCount - 1}
-            </Text>
-            <Text style={{fontSize: SIZES.h3, textAlign: 'center'}}>
-              Chi phí
-            </Text>
+          <View style={styles.centerChart}>
+            <Text style={styles.textH1}>{totalExpenseCount - 1}</Text>
+            <Text style={styles.textH3}>Expenses</Text>
           </View>
         </View>
       );
     }
   };
   const renderCategoryList = () => {
-    const renderItem = ({item}: {item: any}) => (
+    const renderItem = ({item}: {item: Data}) => (
       <TouchableOpacity
         onPress={() => setSelectedCategory(item)}
-        style={{
-          flex: 1,
-          flexDirection: 'row',
-          margin: 5,
-          paddingVertical: SIZES.radius,
-          paddingHorizontal: SIZES.padding,
-          borderRadius: 5,
-          backgroundColor: COLORS.white,
-          ...styles.shadow,
-        }}>
+        style={styles.itemCategory}>
         <Image
           source={item.icon}
-          style={{
-            width: 20,
-            height: 20,
-            tintColor: item.color,
-          }}
+          style={[
+            styles.icon,
+            {
+              tintColor: item.color,
+            },
+          ]}
         />
         <Text
           style={{
@@ -247,6 +220,7 @@ const App: FC<AppProps> = () => {
       <View style={{paddingHorizontal: SIZES.padding - 5}}>
         <Animated.View style={{height: categoryListHeightAnimationValue}}>
           <FlatList
+            showsVerticalScrollIndicator={false}
             data={categories}
             renderItem={renderItem}
             keyExtractor={item => `${item.id}`}
@@ -255,22 +229,18 @@ const App: FC<AppProps> = () => {
         </Animated.View>
 
         <TouchableOpacity
-          style={{
-            flexDirection: 'row',
-            marginVertical: SIZES.base,
-            justifyContent: 'center',
-          }}
+          style={styles.buttonShowMore}
           onPress={() => {
             if (showMoreToggle) {
               Animated.timing(categoryListHeightAnimationValue, {
                 toValue: 115,
-                duration: 500,
+                duration: 300,
                 useNativeDriver: false,
               }).start();
             } else {
               Animated.timing(categoryListHeightAnimationValue, {
                 toValue: 172.5,
-                duration: 500,
+                duration: 300,
                 useNativeDriver: false,
               }).start();
             }
@@ -278,11 +248,11 @@ const App: FC<AppProps> = () => {
             setShowMoreToggle(!showMoreToggle);
           }}>
           <Text style={{fontSize: SIZES.h4}}>
-            {showMoreToggle ? 'Thu gọn' : 'Mở rộng'}
+            {showMoreToggle ? 'LESS' : 'MORE'}
           </Text>
           <Image
             source={showMoreToggle ? icons.up_arrow : icons.down_arrow}
-            style={{marginLeft: 5, width: 15, height: 15, alignSelf: 'center'}}
+            style={styles.iconShowMore}
           />
         </TouchableOpacity>
       </View>
@@ -290,35 +260,33 @@ const App: FC<AppProps> = () => {
   };
   const renderExpenseSummary = () => {
     let data = processCategoryDataToDisplay();
-    console.log('Data-------', data);
     const renderItem = ({item}: {item: any}) => (
       <TouchableOpacity
-        style={{
-          flexDirection: 'row',
-          height: 40,
-          paddingHorizontal: SIZES.radius,
-          borderRadius: 10,
-          backgroundColor:
-            selectedCategory && selectedCategory.name === item.name
-              ? item.color
-              : COLORS.white,
-        }}
+        style={[
+          styles.itemListExpenses,
+          {
+            backgroundColor:
+              selectedCategory && selectedCategory.name === item.name
+                ? item.color
+                : COLORS.white,
+          },
+        ]}
         onPress={() => {
           let categoryName = item.name;
           setSelectCategoryByName(categoryName);
         }}>
         {/* Name/Category */}
-        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+        <View style={styles.base}>
           <View
-            style={{
-              width: 20,
-              height: 20,
-              backgroundColor:
-                selectedCategory && selectedCategory.name === item.name
-                  ? COLORS.white
-                  : item.color,
-              borderRadius: 5,
-            }}
+            style={[
+              styles.dot,
+              {
+                backgroundColor:
+                  selectedCategory && selectedCategory.name === item.name
+                    ? COLORS.white
+                    : item.color,
+              },
+            ]}
           />
 
           <Text
@@ -333,9 +301,8 @@ const App: FC<AppProps> = () => {
             {item.name}
           </Text>
         </View>
-
         {/* Expenses */}
-        <View style={{justifyContent: 'center'}}>
+        <View style={styles.base1}>
           <Text
             style={{
               color:
@@ -353,6 +320,7 @@ const App: FC<AppProps> = () => {
     return (
       <View style={{padding: SIZES.padding}}>
         <FlatList
+          showsVerticalScrollIndicator={false}
           data={data}
           renderItem={renderItem}
           keyExtractor={item => `${item.id}`}
@@ -379,222 +347,56 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84,
     elevation: 3,
   },
+  viewChart: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  base: {flex: 1, flexDirection: 'row', alignItems: 'center'},
+  base1: {justifyContent: 'center'},
+  base2: {
+    flexWrap: 'wrap',
+  },
+  centerChart: {position: 'absolute', top: '42%', left: '42%'},
+  textH1: {fontSize: SIZES.h1, textAlign: 'center'},
+  textH3: {fontSize: SIZES.h3, textAlign: 'center'},
+  svgChartAndroid: {width: '100%', height: 'auto'},
+  itemCategory: {
+    flex: 1,
+    flexDirection: 'row',
+    margin: 5,
+    paddingVertical: SIZES.radius,
+    paddingHorizontal: SIZES.padding,
+    borderRadius: 5,
+    backgroundColor: COLORS.white,
+    shadowColor: COLORS.black,
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 3,
+  },
+  icon: {
+    width: 20,
+    height: 20,
+  },
+  buttonShowMore: {
+    flexDirection: 'row',
+    marginVertical: SIZES.base,
+    justifyContent: 'center',
+  },
+  iconShowMore: {marginLeft: 5, width: 15, height: 15, alignSelf: 'center'},
+  itemListExpenses: {
+    flexDirection: 'row',
+    height: 40,
+    paddingHorizontal: SIZES.radius,
+    borderRadius: 10,
+  },
+  dot: {
+    borderRadius: 5,
+    width: 20,
+    height: 20,
+  },
 });
 export default App;
-const COLORS = {
-  // base colors
-  primary: '#194868', // Dark Blue
-  secondary: '#FF615F', // peach
-
-  // colors
-  black: '#1E1F20',
-  white: '#FFFFFF',
-  lightGray: '#F5F7F9',
-  lightGray2: '#FAFBFD',
-  gray: '#BEC1D2',
-  blue: '#42B0FF',
-  darkgray: '#898C95',
-  yellow: '#FFD573',
-  lightBlue: '#95A9B8',
-  darkgreen: '#008159',
-  peach: '#FF615F',
-  purple: '#8e44ad',
-  red: '#FF0000',
-};
-const confirmStatus = 'C';
-const pendingStatus = 'P';
-const categoriesData = [
-  {
-    id: 1,
-    name: 'Học tập',
-    icon: icons.education,
-    color: COLORS.yellow,
-    expenses: [
-      {
-        id: 1,
-        title: 'Tuition Fee',
-        description: 'Tuition fee',
-        location: "ByProgrammers' tuition center",
-        total: 100.0,
-        status: pendingStatus,
-      },
-      {
-        id: 2,
-        title: 'Arduino',
-        description: 'Hardward',
-        location: "ByProgrammers' tuition center",
-        total: 30.0,
-        status: pendingStatus,
-      },
-      {
-        id: 3,
-        title: 'Javascript Books',
-        description: 'Javascript books',
-        location: "ByProgrammers' Book Store",
-        total: 20.0,
-        status: confirmStatus,
-      },
-      {
-        id: 4,
-        title: 'PHP Books',
-        description: 'PHP books',
-        location: "ByProgrammers' Book Store",
-        total: 20.0,
-        status: confirmStatus,
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: 'Dinh dưỡng',
-    icon: icons.food,
-    color: COLORS.lightBlue,
-    expenses: [
-      {
-        id: 5,
-        title: 'Vitamins',
-        description: 'Vitamin',
-        location: "ByProgrammers' Pharmacy",
-        total: 25.0,
-        status: pendingStatus,
-      },
-
-      {
-        id: 6,
-        title: 'Protein powder',
-        description: 'Protein',
-        location: "ByProgrammers' Pharmacy",
-        total: 50.0,
-        status: confirmStatus,
-      },
-    ],
-  },
-  {
-    id: 3,
-    name: 'Thư giãn',
-    icon: icons.baby_car,
-    color: COLORS.darkgreen,
-    expenses: [
-      {
-        id: 7,
-        title: 'Toys',
-        description: 'toys',
-        location: "ByProgrammers' Toy Store",
-        total: 25.0,
-        status: confirmStatus,
-      },
-      {
-        id: 8,
-        title: 'Baby Car Seat',
-        description: 'Baby Car Seat',
-        location: "ByProgrammers' Baby Care Store",
-        total: 100.0,
-        status: pendingStatus,
-      },
-      {
-        id: 9,
-        title: 'Pampers',
-        description: 'Pampers',
-        location: "ByProgrammers' Supermarket",
-        total: 100.0,
-        status: pendingStatus,
-      },
-      {
-        id: 10,
-        title: 'Baby T-Shirt',
-        description: 'T-Shirt',
-        location: "ByProgrammers' Fashion Store",
-        total: 20.0,
-        status: pendingStatus,
-      },
-    ],
-  },
-  {
-    id: 4,
-    name: 'Làm đẹp',
-    icon: icons.healthcare,
-    color: COLORS.peach,
-    expenses: [
-      {
-        id: 11,
-        title: 'Skin Care product',
-        description: 'skin care',
-        location: "ByProgrammers' Pharmacy",
-        total: 10.0,
-        status: pendingStatus,
-      },
-      {
-        id: 12,
-        title: 'Lotion',
-        description: 'Lotion',
-        location: "ByProgrammers' Pharmacy",
-        total: 50.0,
-        status: confirmStatus,
-      },
-      {
-        id: 13,
-        title: 'Face Mask',
-        description: 'Face Mask',
-        location: "ByProgrammers' Pharmacy",
-        total: 50.0,
-        status: pendingStatus,
-      },
-      {
-        id: 14,
-        title: 'Sunscreen cream',
-        description: 'Sunscreen cream',
-        location: "ByProgrammers' Pharmacy",
-        total: 50.0,
-        status: pendingStatus,
-      },
-    ],
-  },
-  {
-    id: 5,
-    name: 'Thể thao',
-    icon: icons.sports_icon,
-    color: COLORS.purple,
-    expenses: [
-      {
-        id: 15,
-        title: 'Gym Membership',
-        description: 'Monthly Fee',
-        location: "ByProgrammers' Gym",
-        total: 45.0,
-        status: pendingStatus,
-      },
-      {
-        id: 16,
-        title: 'Gloves',
-        description: 'Gym Equipment',
-        location: "ByProgrammers' Gym",
-        total: 15.0,
-        status: confirmStatus,
-      },
-    ],
-  },
-  {
-    id: 6,
-    name: 'Mua sắm',
-    icon: icons.cloth_icon,
-    color: COLORS.red,
-    expenses: [
-      {
-        id: 17,
-        title: 'T-Shirt',
-        description: 'Plain Color T-Shirt',
-        location: "ByProgrammers' Mall",
-        total: 20.0,
-        status: pendingStatus,
-      },
-      {
-        id: 18,
-        title: 'Jeans',
-        description: 'Blue Jeans',
-        location: "ByProgrammers' Mall",
-        total: 50.0,
-        status: confirmStatus,
-      },
-    ],
-  },
-];
