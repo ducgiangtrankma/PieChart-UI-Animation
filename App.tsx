d98d9af1f0592388dc9aa216f775124d1e31a72e
Đức Giang Trần <ducgiangtran@ucs-MacBook-Pro.local>
2021-10-19 15:04:30 +0700

    create

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+interface AppProps {}
+const App: FC<AppProps> = () => {
+  return (
+    <SafeAreaView>
+      <Text>TEST</Text>
+    </SafeAreaView>
+  );
+};
+export default App;
